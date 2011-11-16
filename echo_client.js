529931acc5d798a20ae043d68de7efc0eb2f3d22
Konstantin Käfer <github@kkaefer.com>
2011-11-16 01:28:17 +0100

    allow supplying an argument to echo_client.js

--- echo_client.js
+++ echo_client.js
@@ -3,13 +3,21 @@ var twp = require('./lib/twp');
 
 var api = tdl.fromFile('./misc/echo.tdl');
 
+if (process.argv.length !== 3) {
+    console.warn('Usage: ' + process.env._ + ' "string"');
+    process.exit(1);
+}
+
 var client = new twp.Client(api.protocols.byName['Echo']);
 
 client.connect(80, 'www.dcl.hpi.uni-potsdam.de');
 
 client.on('connect', function() {
+    var string = process.argv[2];
+    console.warn('Measuring "' + string + '"');
+
     client.send('Request', {
-        text: 'foo'
+        text: string
     });
 
     client.on('message', function(name, content) {
