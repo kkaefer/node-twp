9873ba332e5e5bfd28ee0fdaaf3e2dac935addbe
Konstantin Käfer <github@kkaefer.com>
2012-02-05 22:41:20 +0100

    dritte übungsaufgabe

--- calculator_client.js
+++ calculator_client.js
@@ -1,6 +1,10 @@
 var calculator = require('./calculator');
 
-var localhost = new Buffer([127, 0, 0, 1]);
+var parts = process.argv[2].split('.');
+
+// 172.16.59.124
+// var localhost = new Buffer([172, 16, 59, 124]);
+var localhost = new Buffer([parts[0], parts[1], parts[2], parts[3]]);
 
 var expr = {
     host: localhost,
